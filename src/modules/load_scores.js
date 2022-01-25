45e4001b30408eba4f9db6209459ebf514b6c5ef
asanchy <sanchyclips@gmail.com>
2022-01-25 22:04:41 +0200

    Change loadScores regular function to arrow function

--- src/modules/load_scores.js
+++ src/modules/load_scores.js
@@ -1,7 +1,9 @@
-export default function loadScores(htmlContainer, recentScores) {
+const loadScores = (htmlContainer, recentScores) => {
   recentScores.forEach((score) => {
     const listItem = document.createElement('li');
     listItem.textContent = `${score.name}: ${score.score}`;
     htmlContainer.appendChild(listItem);
   });
 }
+
+export default loadScores;
