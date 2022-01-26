6abd98838f4a99c9dcb7a03aedd79250c9249cde
asanchy <sanchyclips@gmail.com>
2022-01-26 21:17:19 +0200

    Create api file for api path components

---
+++ src/modules/api.js
@@ -0,0 +1,10 @@
+const api = {
+  baseUrl: 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/',
+  games: 'games/',
+  id: '',
+  scores: 'scores/',
+}
+
+const {baseUrl,games,id,scores} = api;
+
+export {baseUrl, games, id, scores}
--- src/modules/load_scores.js
+++ src/modules/load_scores.js
@@ -1,9 +1,12 @@
-const loadScores = (htmlContainer, recentScores) => {
-  recentScores.forEach((score) => {
-    const listItem = document.createElement('li');
-    listItem.textContent = `${score.name}: ${score.score}`;
-    htmlContainer.appendChild(listItem);
-  });
+const loadScores = (htmlContainer, scores) => {
+  scores().then((value) => {
+    value.forEach((score) => {
+      const listItem = document.createElement('li');
+      listItem.textContent = `${score.user}: ${score.score}`;
+      htmlContainer.appendChild(listItem);
+    });
+  })
+  
 };
 
 export default loadScores;
